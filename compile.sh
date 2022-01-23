6ef19a4e1755c09ff31dccf07abf4819431641b4
Robert Obkircher <robert.obkircher@gmail.com>
2022-01-24 00:21:32 +0100

    fix compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ -o posture_reminder -I/usr/include/opencv4 main.cpp -lopencv_core -lopencv_objdetect -lopencv_videoio -lopencv_highgui -pthread -lpulse-simple -lpulse -lopencv_imgproc
+g++ -o posture_reminder -DHAS_PULSE -I/usr/include/opencv4 main.cpp -lopencv_core -lopencv_objdetect -lopencv_videoio -lopencv_highgui -pthread -lpulse-simple -lpulse -lopencv_imgproc
